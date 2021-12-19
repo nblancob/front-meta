c373968d75c3e6fca0da7bf77d046c516b97e3bf
nblancob <nblancob@unal.edu.co>
2021-12-18 23:33:23 -0500

    login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,24 +1,36 @@
 import Card from "react-bootstrap/Card";
 import home from "../styles/Home.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
-const Home = () =>{
+const Home = () => {
   return (
     <div className="container" style={{ home }}>
       <div className="abs-center">
         <Card className="card">
-          <Card.Title className="text-center">
-            BIENVENIDO
-          </Card.Title>
           <Card.Img
             variant="top"
             className="image"
             src="https://image.freepik.com/vector-gratis/concepto-innovacion-dibujado-mano-cohete_23-2149164445.jpg"
           />
-
           <Card.Body>
-            <Card.Title className="title_card text-white">
-              Ingresa con Google
-            </Card.Title>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
           </Card.Body>
         </Card>
       </div>
