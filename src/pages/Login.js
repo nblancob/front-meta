509caf2901274b54084aafd532f563ce0fd26059
nblancob <nblancob@unal.edu.co>
2021-12-29 23:07:51 -0500

    coding sign in

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,8 +3,37 @@ import home from "../styles/Home.css";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
+import { gql, useLazyQuery } from "@apollo/client";
+import { useState } from "react";
+
+const QUERY_AUTHUSER = gql`
+  query authUser($email: String!, $password: String!) {
+    authUser(input: { email: $email, password: $password })
+  }
+`;
 
 const Home = () => {
+  const [User, SetUser] = useState({
+    email: " ",
+    password: " ",
+  });
+  const [authUser, data] = useLazyQuery(QUERY_AUTHUSER);
+
+  const onClick = () => {
+    authUser({
+      variables: { email: User.email, password: User.password },
+    });
+    if (data.data.authUser === true) {
+      console.log("WELCOME TO THE JUNGLE");
+    } else {
+      console.log("GET OUT FUCK YOU");
+    }
+  };
+
+  const onChange = (event) => {
+    SetUser({ ...User, [event.target.name]: event.target.value });
+  };
+
   return (
     <div className="container" style={{ home }}>
       <div className="abs-center">
@@ -18,17 +47,32 @@ const Home = () => {
             <Form>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  name="email"
+                  onChange={onChange}
+                />
+                <Form.Text className="text-white">
                   We'll never share your email with anyone else.
                 </Form.Text>
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  onChange={onChange}
+                />
               </Form.Group>
-              <Button className="ingresar-btn" variant="success" type="submit">
+              <Button
+                className="ingresar-btn"
+                variant="success"
+                // type="submit"
+                onClick={onClick}
+              >
                 Ingresar
               </Button>
               <Link to="/registrer">
